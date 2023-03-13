783eb7f42823eb9062e9ed9731867bb1e6469f28
LukeAnger <anger.luke@gmail.com>
2023-03-13 17:23:00 -0400

    added contact form reset on submit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = ({styles = {}}) => {
       <div className='contact pa' style={styles}>
         <div className='contactTitle  holoText' style={{animation: 'fadeIn 2s ease-in forwards 1s, holoText 5s infinite ease-in-out alternate'}}>Contact</div>
         {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeKJkqFkFXA9ujJbEqh424tmy7vFFJhUM3Rk_ggKjAjigkFzg/viewform?embedded=true" width="640" height="809" frameborder="0" marginHeight="0" marginWidth="0">Loading…</iframe> */}
-        {GoogleForm()}
+        <GoogleForm />
       </div>
     </>
   )
--- src/components/contact/GoogleForm.jsx
+++ src/components/contact/GoogleForm.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const formDataInit = {
+  Name: '',
+  Email: '',
+  Suggestions: '',
+}
 
 const GoogleForm = () => {
+
+  const [formData, setFormData] = useState(formDataInit)
+
+  const handleChange = (e) => {
+    let tar = e.currentTarget
+    let name = tar.name
+    let value = tar.value
+    setFormData({ ...formData, [name]: value })
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -16,22 +31,25 @@ const GoogleForm = () => {
             console.log('res', res)
             res.json()
           })
+    setFormData(formDataInit)
   }
 
   return (
     <form id='suggestionsForm' onSubmit={handleSubmit} method="post" name='google-sheet' action='https://script.google.com/macros/s/AKfycbz7iomyh4iYcCTpHfEFIRJIhrGogyjE2NW1m1Whcc08XcEfBAMWyQB6VArqAcG6c1jU/exec' >
         <label className='fr' >Name:
-        <input className='sugForm1' name='Name' type='text' placeholder='name' ></input>
+        <input className='sugForm1' onChange={handleChange} name='Name' type='text' value={formData.Name} placeholder='name' ></input>
         </label>
 
 
         <label className='fr'>Email:
-        <input className='sugForm2' name='Email' type='email' placeholder='email' ></input>
+        <input className='sugForm2' onChange={handleChange} name='Email' type='email' value={formData.Email} placeholder='email' ></input>
         </label>
 
 
         <label className='fc'>Suggestion/Greeting
-        <textarea className='sugForm3' name='Suggestions' type='text' placeholder='Leave me some feedback or just say hello!' required></textarea>
+        <textarea className='sugForm3' onChange={handleChange} name='Suggestions' type='text'
+        value={formData.Suggestions}
+         placeholder='Leave me some feedback or just say hello!' required></textarea>
         </label>
 
 
